981d8b71fe4f3d2161deb957c9ca8ffe4a899747
Yassine Belmahfoud <yassinebelmahfoud@gmail.com>
2022-06-06 17:11:51 +0200

    feat: add decorator to bind property when calling callback

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
+function Bounder(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    console.log("originalMethod", originalMethod);
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return originalMethod.bind(this);
+        },
+    };
+    console.log("adjDescriptor", adjDescriptor);
+    return adjDescriptor;
+}
+
 class FormInput {
     templateInput: HTMLTemplateElement;
     divOutput: HTMLElement;
     form: HTMLFormElement;
+
+    formInputs: {
+        title: HTMLInputElement;
+        description: HTMLInputElement;
+        people: HTMLInputElement;
+    };
 
     constructor() {
         this.templateInput = document.getElementById(
@@ -14,13 +34,29 @@ class FormInput {
             true
         );
         this.form = copyTemplateInput.firstElementChild as HTMLFormElement;
+        this.form.id = "user-input";
+        this.formInputs = {
+            title: <HTMLInputElement>this.form.querySelector("#title"),
+            description: <HTMLInputElement>this.form.querySelector("#description"),
+            people: <HTMLInputElement>this.form.querySelector("#people"),
+        };
+
+        this.form.addEventListener("submit", this.handleSubmit);
+
         this.joinTo();
     }
 
     private joinTo() {
         this.divOutput.insertAdjacentElement("afterbegin", this.form);
     }
+
+    @Bounder
+    private handleSubmit(e: Event) {
+        console.log("*******");
+        e.preventDefault();
+
+        console.log("formInputs", this.formInputs.title.value);
+    }
 }
 
 const formInput = new FormInput();
-
